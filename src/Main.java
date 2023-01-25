e9869c4349d427c54308ae10a01b9da8431511b1
Mike LeBlanc <mike_leblanc232@hotmail.com>
2023-01-25 13:00:17 -0400

    changed to for loops instead of do whiles. im tired.

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,16 @@ public class Main {
         Scanner input = new Scanner(System.in);
         DecimalFormat dec = new DecimalFormat("0.00");
 
-        // arrays (assuming only 3 people are signing up for this league.)
-        Team[] leagueRoster = new Team[3];
-        Player[] teamLineup = new Player[3];
+        // 2d array (assuming only 3 people are signing up for this league and 3 players per team.)
+        Team[][] leagueRoster = new Team[3][4];
+
 
         // capture all user input for teams.
         System.out.println("FANTASY HOCKEY APPLICATION");
         System.out.println("TEAM ENTRY");
         System.out.println("=====================================");
         for (int i = 0; i < leagueRoster.length; ++i) {
-            String name;
+            String teamName;
 
             do {
                 System.out.println("Please enter your teams name: ");
@@ -23,53 +23,35 @@ public class Main {
                     System.out.println("Please enter a valid name!");
                     input.next();
                 }
-                name = input.nextLine();
-                leagueRoster[i] = new Team(name);
+                teamName = input.nextLine();
+                leagueRoster[i][0] = new Team(teamName);
             }
-            while (input == null || input.equals(""));
+            while (input == null || input.equals("") || teamName.length() > 3);
         }
 
         // time for the players. name, goals and assists. Salary is randomly set when players are created.
         System.out.println("PLAYER ENTRY");
         System.out.println("=====================================");
-        for (int i = 0; i < teamLineup.length; ++i) {
-            String name;
-            int goals;
-            int assists;
 
-            //maybe do this in a nested for loop. might be cleaner.
-            System.out.println("Enter Players for " + leagueRoster[i].getName() + ".");
-            do {
-                System.out.println("Enter name for player #" + (i+1) +":" );
-                while (!input.hasNextLine()){
-                    System.out.println("Please enter a valid name!");
-                    input.next();
+        String playerName;
+
+        for (int row = 0; row < leagueRoster.length; row++) {
+            System.out.println("Please enter the players for " + leagueRoster[row][0] + ".");
+            for (int col = 1; col < leagueRoster[0].length; col++) {
+                if (col == 0) {
+                    col++;
                 }
-                name = input.nextLine();
-
-                System.out.println("Enter number of goals for "+ name +":");
-                while (!input.hasNextInt()) { //Check that the value is an integer
-                    System.out.println("Enter a positive number.");
-                    input.nextInt();
-                    input.nextLine();
+                do {
+                    System.out.println("Enter name for player #" + (col) + ":");
+                    while (!input.hasNextLine()) {
+                        System.out.println("Please enter a valid name!");
+                        input.next();
+                    }
+                    playerName = input.nextLine();
                 }
-                goals = input.nextInt();
-                input.nextLine();
-
-                System.out.println("Enter number of assists for "+ name +":");
-                while (!input.hasNextInt()) { //Check that the value is an integer
-                    System.out.println("Enter a positive number.");
-                    input.nextInt();
-                    input.nextLine();
-                }
-                assists = input.nextInt();
-                input.nextLine();
-
-                teamLineup[i] = new Player(name, goals, assists);
+                while (input == null || input.equals("") || goals < 0 || assists < 0 || playerName.length > 3);
             }
-            while (input == null || input.equals("") || goals < 0 || assists < 0);
         }
-
         // output all data here. Learn some proper formatting, noob.
     }
 }
